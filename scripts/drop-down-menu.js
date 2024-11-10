c97f62b322156fbd7fbbe658bb4b82b5c304d845
czenturion <czenturijon@email.com>
2024-11-10 23:27:05 +0300

    feat: drop down menu, tea exploring section started to develop

--- scripts/drop-down-menu.js
+++ scripts/drop-down-menu.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.drop-down-menu-btn');
-    const teaSortsMenu = document.querySelector('.tea-sorts');
+    const toggleBtn = document.querySelector('.drop-down-menu-btn');
+    const closeBtn = document.querySelector('.drop-down-menu-close-btn');
+    const teaSortsMenu = document.querySelector('.tea-sorts-drop-down-menu');
 
-    toggleButton.addEventListener('click', () => {
+    toggleBtn.addEventListener('click', () => {
         console.log('+++++')
         teaSortsMenu.classList.add('show');
     });
 
+    closeBtn.addEventListener('click', () => {
+        console.log('-----')
+        teaSortsMenu.classList.remove('show');
+    })
+
     window.addEventListener('click', (event) => {
-        if (!teaSortsMenu.contains(event.target) && event.target !== toggleButton) {
+        if (!teaSortsMenu.contains(event.target) && event.target !== toggleBtn) {
             teaSortsMenu.classList.remove('show');
         }
     });
